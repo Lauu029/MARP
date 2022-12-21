ce484a5ba1afbe535b4d63a4d3efcfbe50136dc7
Laura <lgomez25@ucm.es>
2022-12-21 12:46:52 +0100

    ._.

--- Ejercicios Juez/EJ0/1.NumeroCasos.cpp
+++ Ejercicios Juez/EJ0/1.NumeroCasos.cpp
@@ -8,72 +8,112 @@
 #include <iostream>
 #include <fstream>
 #include <map>
+#include <math.h>
 #include <vector>
-#include "TreeSet_AVL.h"  // propios o los de las estructuras de datos de clase
+#include "bintree.h"  // propios o los de las estructuras de datos de clase
 
 using namespace std;
-/*@ <answer>
+///*@ <answer>
+//
+// Escribe aquí un comentario general sobre la solución, explicando cómo
+// se resuelve el problema y cuál es el coste de la solución, en función
+// del tamaño del problema.
+//
+// @ </answer> */
+//
+//
+// // ================================================================
+// // Escribe el código completo de tu solución aquí debajo
+// // ================================================================
+// //@ <answer>
+//
+//
+//bool esAVL(const bintree<int>& caso, bool& noEs, int& alt) {
+//	if (noEs == false) return false;
+//	if (caso.empty()) return true;
+//	if (caso.left().empty() && caso.right().empty()) {
+//		alt = 1;
+//		return true;
+//	}
+//
+//	int izq = 0, der = 0;
+//	bool izqAVL = true, derAVL = true;
+//	if (!caso.left().empty()) {
+//
+//
+//
+//		izqAVL = esAVL(caso.left(), noEs, izq);
+//	}
+//	if (!caso.right().empty()) {
+//		derAVL = esAVL(caso.right(), noEs, der);
+//	}
+//	alt = 1 + max(izq, der);
+//	noEs = (abs(izq - der) <= 1) && izqAVL && derAVL;
+//	return noEs;
+//}
+//bool esBinario(const bintree<int>& caso) {
+//	if (caso.empty())return true;
+//	vector<int> inorden = caso.inorder();
+//	int i = 0;
+//	while (i + 1 < inorden.size())
+//	{
+//		if (inorden[i] >= inorden[i + 1]) return false;
+//		++i;
+//	}
+//	bool avl = true;
+//	int alt = 0;
+//	return esAVL(caso, avl, alt);
+//}
+//void resuelveCaso() {
+//
+//	// leer los datos de la entrada
+//	bintree<int> caso;
+//	caso = leerArbol(-1);
+//	// resolver el caso posiblemente llamando a otras funciones
+//	if (esBinario(caso)) cout << "SI\n";
+//	else cout << "NO\n";
+//	// escribir la solución
+//}
 
- Escribe aquí un comentario general sobre la solución, explicando cómo
- se resuelve el problema y cuál es el coste de la solución, en función
- del tamaño del problema.
-
- @ </answer> */
-
-
- // ================================================================
- // Escribe el código completo de tu solución aquí debajo
- // ================================================================
- //@ <answer>
-
-
-bool esAVL(const bintree<int>& caso, bool& noEs, int& alt) {
-	if (noEs == false) return false;
-	if (caso.empty()) return true;
-	if (caso.left().empty() && caso.right().empty()) {
-		alt = 1;
-		return true;
-	}
-
-	int izq = 0, der = 0;
-	bool izqAVL = true, derAVL = true;
-	if (!caso.left().empty()) {
-
-
-
-		izqAVL = esAVL(caso.left(), noEs, izq);
-	}
-	if (!caso.right().empty()) {
-		derAVL = esAVL(caso.right(), noEs, der);
-	}
-	alt = 1 + max(izq, der);
-	noEs = (abs(izq - der) <= 1) && izqAVL && derAVL;
-	return noEs;
-}
-bool esBinario(const bintree<int>& caso) {
-	if (caso.empty())return true;
-	vector<int> inorden = caso.inorder();
+//bool esAVL(bintree<int> arbol, int& h, int& hi, int& hd) {
+//
+//	if (arbol.right().empty() && arbol.left().empty()) {
+//		return true;
+//	}
+//	if (arbol.right().empty()) {
+//		hi++;
+//		return esAVL(arbol.left(), hi);
+//	}
+//	else if (arbol.left().empty()) {
+//		hd++;
+//		return esAVL(arbol.right(), hd);
+//	}
+//	else {
+//		hi++; hd++;
+//		bool es = (esAVL(arbol.right(), hd) && esAVL(arbol.left(), hi));
+//		return es && abs(hi - hd) <= 1;
+//	}
+//
+//}
+bool esBinario(bintree<int>& arbol) {
+	vector<int> in = arbol.inorder();
 	int i = 0;
-	while (i + 1 < inorden.size())
-	{
-		if (inorden[i] >= inorden[i + 1]) return false;
-		++i;
+	bool bin = true;
+	while (i < in.size() - 1 && bin) {
+		bin = in[i] < in[i + 1];
+		i++;
 	}
-	bool avl = true;
-	int alt = 0;
-	return esAVL(caso, avl, alt);
+	return bin;
 }
 void resuelveCaso() {
+	bintree<int> arbol = leerArbol(-1);
+	int h = 0;
+	if (esBinario(arbol)) {
+		cout << "SI\n";
 
-	// leer los datos de la entrada
-	bintree<int> caso;
-	caso = leerArbol(-1);
-	// resolver el caso posiblemente llamando a otras funciones
-	if (esBinario(caso)) cout << "SI\n";
+	}
 	else cout << "NO\n";
-	// escribir la solución
 }
-
 //@ </answer>
 //  Lo que se escriba dejado de esta línea ya no forma parte de la solución.
 
